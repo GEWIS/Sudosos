925e29e7cc30ff3cac3c3ca242428fd38dff5dca
Wouters <r.a.d.wouters@student.tue.nl>
2017-07-15 13:38:14 +0200

    Frontend boilerplate

---
+++ public/js/services.js
@@ -0,0 +1,4 @@
+/**
+ * Created by s156386 on 15-7-2017.
+ */
+angular.module('sudosos.services', []);
---
+++ public/js/sudosos.js
@@ -0,0 +1,3 @@
+angular.module('sudosos', ['sudosos.controllers', 'sudosos.filters', 'sudosos.services', 'ui.router'])
+    .config(function ($stateProvider) {
+    });
