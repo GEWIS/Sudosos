1b382032c8d4acded2e1d08f2d7f8b03b778eed3
Justin Szanto <jszanto@users.noreply.github.com>
2017-07-17 13:02:35 +0200

    Implement login via de GEWIS website

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -25,7 +27,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/';
 
     /**
      * Create a new controller instance.
@@ -35,5 +37,36 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    public function index()
+    {
+        return View('auth.index');
+    }
+
+    public function showGEWISLogin()
+    {
+        // TODO: check if user is logged in already
+        return redirect()->to(env('GEWISWEB_AUTH_URL'));
+    }
+
+    public function doGEWISLogin(Request $request)
+    {
+        if (Auth::attempt(['jwt_token' => $request->token])) {
+            // Authentication passed...
+            return redirect()->intended('/');
+        }
+
+        //TODO: return some error
+    }
+
+    public function showExternalLogin()
+    {
+
+    }
+
+    public function doExternalLogin()
+    {
+
     }
 }
