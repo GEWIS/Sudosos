124f304ac455179f9053e9a3da9f98a840e0259f
lvisser <l.leroy.visser@gmail.com>
2017-07-26 13:33:28 +0200

    Hotfixes Controllers documentation

--- app/Models/PointOfSale.php
+++ app/Models/PointOfSale.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 /**
  * @SWG\Definition(
- *      definition="PointOfSale",
+ *      definition="pointOfSale",
  *      required={"id","owner_id","name"},
  *      @SWG\Property(
  *             property="id",
@@ -49,7 +49,7 @@ use Illuminate\Database\Eloquent\Model;
  *               required={"owner_id", "name"},
  *      @SWG\Property(
  *             property="owner_id",
- *             type="string",
+ *             type="integer",
  *             description="Id of the owner from this Point of Sale"
  *         ),
  *      @SWG\Property(
