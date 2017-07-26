fa4796b2a5a54996e330d6eb2e65be2140a08ab5
Wouters <r.a.d.wouters@student.tue.nl>
2017-07-26 14:31:14 +0200

    Made everything work with the new API calls

--- www/js/angular/controllers.js
+++ www/js/angular/controllers.js
@@ -7,9 +7,14 @@ angular.module('sudosos.controllers', [])
             name: "Ge Bruiker",
             currentBalance: 42.69,
             committees: [
-                "BAC",
-                "GEILER",
-                "EJC17-34"
+                {
+                    id: 2,
+                    name: "BAC"
+                },
+                {
+                    id: 9,
+                    name: "GEILER"
+                }
             ]
         };
 
@@ -24,26 +29,27 @@ angular.module('sudosos.controllers', [])
         $scope.transferAmount = {
             amount: 0
         };
+        $scope.$watch('currentCommittee.committee', function () {
+            $scope.loadingProducts = $http.get(rootUrl + '/api/v1/products').then(function (response) {
+                $scope.products = response.data;
+                for (var i = 0; i < $scope.products.length; i++) {
+                    $scope.products[i].amount = parseInt(Math.random() * 100);
+                }
+            });
 
-        $scope.loadingProducts = $http.get(rootUrl + '/api/v1/products').then(function (response) {
-            $scope.products = response.data;
-            for(var i = 0; i < $scope.products.length; i++){
-                $scope.products[i].amount = parseInt(Math.random() * 100);
-            }
-        });
+            $scope.loadingStorages = $http.get(rootUrl + '/api/v1/storages').then(function (response) {
+                $scope.storages = response.data;
+                for (var i = 0; i < $scope.storages.length; i++) {
+                    $scope.storages[i].items = [];
+                }
+            });
 
-        $scope.loadingStorages = $http.get(rootUrl + '/api/v1/storages').then(function (response) {
-            $scope.storages = response.data;
-            for(var i = 0; i < $scope.storages.length; i++){
-                $scope.storages[i].items = [];
-            }
-        });
-
-        $scope.loadingPOS = $http.get(rootUrl + '/api/v1/pointsofsale').then(function (response) {
-            $scope.pointsOfSale = response.data;
-            for(var i = 0; i < $scope.pointsOfSale.length; i++){
-                $scope.pointsOfSale[i].storages = [];
-            }
+            $scope.loadingPOS = $http.get(rootUrl + '/api/v1/pointsofsale').then(function (response) {
+                $scope.pointsOfSale = response.data;
+                for (var i = 0; i < $scope.pointsOfSale.length; i++) {
+                    $scope.pointsOfSale[i].storages = [];
+                }
+            });
         });
 
         $scope.dropStorage = function ($data, $event) {
@@ -58,7 +64,7 @@ angular.module('sudosos.controllers', [])
             }
             this.pointOfSale.storages.push($data);
         };
-        
+
         $scope.dropProduct = function ($data, $event) {
             for(var i = 0; i < this.storage.items.length; i++){
                 if(this.storage.items[i] == $data){
@@ -86,59 +92,61 @@ angular.module('sudosos.controllers', [])
             $scope.currentStorage.items.push($scope.itemToAdd);
             $scope.editModal.close();
         };
-        
+
     }])
     .controller('ProductsCtrl',['$scope', '$http', '$uibModal', 'rootUrl',
         function ($scope, $http, $uibModal, rootUrl) {
-        $scope.selectedIndex = -1;
-        $scope.searchTerm = "";
-        $scope.searchBy = "name";
-        $scope.imageSrc = "http://gewis.nl/willem";
-        $scope.categories = ["other", "ticket", "food"];
+            $scope.selectedIndex = -1;
+            $scope.searchTerm = "";
+            $scope.searchBy = "name";
+            $scope.imageSrc = "http://gewis.nl/willem";
+            $scope.categories = ["other", "ticket", "food"];
 
-        $scope.filterProducts = function (value, index, array) {
-          if(value[$scope.searchBy].toString().toLowerCase().indexOf($scope.searchTerm.toString().toLowerCase()) != -1){
-            return true;
-          }
-        };
+            $scope.filterProducts = function (value, index, array) {
+                if(value[$scope.searchBy].toString().toLowerCase().indexOf($scope.searchTerm.toString().toLowerCase()) != -1){
+                    return true;
+                }
+            };
 
-        $scope.startEditing = function (item) {
-            item.editing = true;
-        };
+            $scope.startEditing = function (item) {
+                item.editing = true;
+            };
 
-        $scope.stopEditing = function (item) {
-            item.editing = false;
-        };
+            $scope.stopEditing = function (item) {
+                item.editing = false;
+            };
 
-        $scope.editProduct = function () {
-            $scope.selectedProduct = $scope.products[$scope.selectedIndex];
-            $scope.editModal = $uibModal.open({
-                templateUrl: 'templates/editProductModalTemplate.html',
-                size: 'sm',
-                scope: $scope
+            $scope.editProduct = function () {
+                $scope.selectedProduct = $scope.products[$scope.selectedIndex];
+                $scope.editModal = $uibModal.open({
+                    templateUrl: 'templates/editProductModalTemplate.html',
+                    size: 'sm',
+                    scope: $scope
+                });
+            };
+
+            $scope.closeModal = function () {
+                $scope.editModal.close();
+            };
+
+            $scope.selectItem = function (index) {
+                if($scope.selectedIndex == index){
+                    $scope.selectedIndex = -1;
+                }else{
+                    $scope.selectedIndex = index;
+                }
+            };
+            $scope.$watch('currentCommittee.committee', function () {
+                $scope.loadingData = $http.get(rootUrl + '/api/v1/products/owner/' + $scope.currentCommittee.committee.id)
+                    .then(function (response) {
+                        $scope.products = response.data;
+                        for(var i = 0; i < $scope.products.length; i++){
+                            $scope.editing = false;
+                        }
+                    });
             });
-        };
 
-        $scope.closeModal = function () {
-            $scope.editModal.close();
-        };
-
-        $scope.selectItem = function (index) {
-          if($scope.selectedIndex == index){
-              $scope.selectedIndex = -1;
-          }else{
-              $scope.selectedIndex = index;
-          }
-        };
-
-        $scope.loadingData = $http.get(rootUrl + '/api/v1/products').then(function (response) {
-            $scope.products = response.data;
-            for(var i = 0; i < $scope.products.length; i++){
-                $scope.editing = false;
-            }
-        });
-
-    }])
+        }])
     .controller('FinancialCtrl', ['$scope', function ($scope) {
 
     }])
