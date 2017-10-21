5bf40757ddb2cc2d3420bfaf684b4e57edb4b8e5
Wouters <r.a.d.wouters@student.tue.nl>
2017-10-22 00:30:28 +0200

    Fixed the moving of items from storage to storage

--- www/js/angular/controllers.js
+++ www/js/angular/controllers.js
@@ -27,7 +27,8 @@ angular.module('sudosos.controllers', [])
     }])
     .controller('ManageCtrl', ['$scope', '$http', '$uibModal', 'rootUrl', function ($scope, $http, $uibModal, rootUrl) {
         $scope.transferAmount = {
-            amount: 0
+            amount: 0,
+            trays: 0
         };
 
         $scope.searchTerm = "";
@@ -35,9 +36,6 @@ angular.module('sudosos.controllers', [])
             var currentCommitteeId = $scope.currentCommittee.committee.id;
             $scope.loadingProducts = $http.get(rootUrl + '/products/owner/' + currentCommitteeId).then(function (response) {
                 $scope.products = response.data;
-                for (var i = 0; i < $scope.products.length; i++) {
-                    $scope.products[i].amount = parseInt(Math.random() * 100);
-                }
             });
 
             // Load the storages
@@ -51,6 +49,11 @@ angular.module('sudosos.controllers', [])
                     (function (index) {
                         var promise = $http.get(rootUrl + '/storages/' + $scope.storages[index].id + "/stores").then(function (response) {
                             $scope.storages[index].items = response.data;
+                            var items = $scope.storages[index].items;
+                            for(var j = 0; j < items.length; j++){
+                                items[j].editing = false;
+                                items[j].storage = $scope.storages[index];
+                            }
                         });
                         $scope.storagePromises.push(promise);
                     })(i);
@@ -72,8 +75,11 @@ angular.module('sudosos.controllers', [])
 
         $scope.dropStorage = function ($data, $event) {
             // If the dropped object has the category attribute, it is not a storage.
-            if($data.category != null){
+            if($data.category !== null){
                 return;
+            }
+            if(this.pointOfSale.storages === null){
+                
             }
             for(var i = 0; i < this.pointOfSale.storages.length; i++){
                 if(this.pointOfSale.storages[i] == $data){
@@ -83,14 +89,23 @@ angular.module('sudosos.controllers', [])
             this.pointOfSale.storages.push($data);
         };
 
-        $scope.dropProduct = function ($data, $event) {
+        $scope.dropProduct = function ($data, storage) {
             for(var i = 0; i < this.storage.items.length; i++){
                 if(this.storage.items[i] == $data){
                     return;
                 }
             }
+            // Check if items are transferred from one storage to another or not
+            $scope.storageToStorage = $data.hasOwnProperty('pivot');
+
+            // Reset the counters for the amount of trays/items to transfer
+            $scope.transferAmount = {
+                amount: 0,
+                trays: 0
+            };
+
             $scope.currentProduct = $data;
-            $scope.currentStorage = this.storage;
+            $scope.currentStorage = storage;
             $scope.editModal = $uibModal.open({
                 templateUrl: 'templates/transferAmountModalTemplate.html',
                 size: 'sm',
@@ -106,36 +121,102 @@ angular.module('sudosos.controllers', [])
                 return;
             }
 
-            // Check if the same object is already in the list: If this is the case, increase the amount
+
+            // Check if the same object is already in the storage: If this is the case, increase the amount
             for(var i = 0; i < $scope.currentStorage.items.length; i++){
                 var currentItem = $scope.currentStorage.items[i];
-                if(currentItem.id == $scope.currentProduct.id){
-                    currentItem.amount += $scope.transferAmount.amount;
+                currentItem.storage = $scope.currentStorage;
+                if(currentItem.id === $scope.currentProduct.id){
+
+                    // If a product is transferred from a storage to a storage, decrease the stock of the source storage
+                    if($scope.storageToStorage){
+                        $scope.currentProduct.pivot.stock -= $scope.transferAmount.amount;
+                        $http.put(rootUrl + '/storages/' + $scope.currentProduct.storage.id + '/stock/' + $scope.currentProduct.id,
+                            {
+                                value: $scope.currentProduct.pivot.stock
+                            }
+                        ).then(function (response) {
+
+                        });
+                    }
+
                     $http.put(rootUrl + "/storages/" + $scope.currentStorage.id + "/stock/" + currentItem.id,
                         {
-                            value: $scope.transferAmount.amount
+                            value: currentItem.pivot.stock + $scope.transferAmount.amount
                         }
                     ).then(function (response) {
-                        console.log(response);
+                        // Close the edit modal
                         $scope.editModal.close();
                     });
+                    currentItem.pivot.stock += $scope.transferAmount.amount;
                     return;
                 }
             }
 
+            // If the item is not yet present in the source storage, this code gets executed
+            // Decrease the stock of the source item if storage to storage
+            if($scope.storageToStorage){
+                $scope.currentProduct.pivot.stock -= $scope.transferAmount.amount;
+                $http.put(rootUrl + '/storages/' + $scope.currentProduct.storage.id + '/stock/' + $scope.currentProduct.id,
+                    {
+                        value: $scope.currentProduct.pivot.stock
+                    }
+                ).then(function (response) {
+
+                });
+            }
+            // Add the item
             $http.post(rootUrl + "/storages/" + $scope.currentStorage.id + "/stores/" + $scope.currentProduct.id,
                 {
                     value: $scope.transferAmount.amount
                 }
             ).then(function (response) {
-                console.log(response);
-                $scope.currentProduct.amount = $scope.currentProduct.amount - $scope.transferAmount.amount;
+                // Copy the item so that the properties adjust do not change at the original item
                 $scope.itemToAdd = angular.copy($scope.currentProduct);
-                $scope.itemToAdd.amount = $scope.transferAmount.amount;
+                $scope.itemToAdd.pivot = {
+                    stock: 0
+                };
+
+                $scope.itemToAdd.editing = false;
+                $scope.itemToAdd.pivot.stock = $scope.transferAmount.amount;
+
                 $scope.currentStorage.items.push($scope.itemToAdd);
+
                 $scope.editModal.close();
             });
+            // Check if all items of a product are transferred
+            if($scope.storageToStorage && $scope.transferAmount.amount === $scope.currentProduct.stock){
+                for(var i = 0; i < $scope.currentProduct.storage.items; i++){
+                    if($scope.currentProduct.storage.items[i].id === $scope.currentProduct.id){
+                        // Delete the product from the storage on the server
+                        $http.delete(rootUrl + '/storages/' + $scope.currentProduct.storage.id + '/stores/' + $scope.currentProduct.id)
+                            .then(function (response) {
+                                // Remove the item in the original storage and set the ite
+                                $scope.currentProduct.storage.items.splice(i, 1);
+                                $scope.itemToAdd.storage = $scope.currentStorage;
+                                $scope.currentStorage.items.push($scope.itemToAdd);
+                            });
+                    }
+                }
+            }
         };
+
+        // This function is needed to automatically add multiply the amount of trays to get the amount of items
+        $scope.addTrays = function () {
+            $scope.transferAmount.amount = $scope.transferAmount.trays * $scope.currentProduct.tray_size;
+        };
+
+        // The function that is called when the input field is blurred
+        $scope.stopEditing = function (item, storage) {
+            item.editing = false;
+            // Update the value in the database
+            $http.put(rootUrl + "/storages/" + storage.id + "/stock/" + item.id,
+                {
+                    value: item.pivot.stock
+                }
+            ).then(function (response) {
+            });
+        }
     }])
     .controller('ProductsCtrl',['$scope', '$http', '$uibModal', 'rootUrl',
         function ($scope, $http, $uibModal, rootUrl) {
